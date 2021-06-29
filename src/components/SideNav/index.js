8f304e217dba706fc4b243283ae14af9d36b3e62
PolyCenter <86454867+polycenter@users.noreply.github.com>
2021-06-29 11:58:35 +0700

    up

--- src/components/SideNav/index.js
+++ src/components/SideNav/index.js
@@ -163,17 +163,17 @@ function SideNav({ history }) {
           </AutoColumn>
           <AutoColumn gap="0.5rem" style={{ marginLeft: '.75rem', marginBottom: '4rem' }}>
             <HeaderText>
-              <Link href="https://pancakeswap.finance/" target="_blank">
-                PancakeSwap
+              <Link href="https://polycenter.io/" target="_blank">
+                PolyCenter
               </Link>
             </HeaderText>
             <HeaderText>
-              <Link href="https://docs.pancakeswap.finance/" target="_blank">
+              <Link href="https://medium.com/@PolyCenter" target="_blank">
                 Docs
               </Link>
             </HeaderText>
             <HeaderText>
-              <Link href="https://twitter.com/PancakeSwap " target="_blank">
+              <Link href="https://twitter.com/Poly_Center" target="_blank">
                 Twitter
               </Link>
             </HeaderText>
