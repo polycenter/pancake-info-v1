fd6185435e6d0d747ad78591f0cca8330f2a7f95
PolyCenter <86454867+polycenter@users.noreply.github.com>
2021-06-29 11:50:14 +0700

    up

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,9 +4,9 @@ import { Flex } from 'rebass'
 import Link from '../Link'
 
 const links = [
-  { url: 'https://pancakeswap.finance', text: 'About' },
-  { url: 'https://docs.pancakeswap.finance', text: 'Docs' },
-  { url: 'https://github.com/pancakeswap/pancakeswap-info', text: 'Code' },
+  { url: 'https://polycenter.io', text: 'About' },
+  { url: 'https://medium.com/@PolyCenter', text: 'Docs' },
+  { url: 'https://github.com/polycenterio', text: 'Code' },
 ]
 
 const FooterLink = ({ children, ...rest }) => (
